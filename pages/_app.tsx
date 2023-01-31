4fd5b91482e071eb418b24496118cb0e9bd5339d
snroeung <sarenna.roeung@gmail.com>
2023-01-31 16:34:08 -0500

    added next-auth signin, access tokens generation, and accounts info api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
+import { AppProps } from 'next/app';
+import { Session } from 'next-auth';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function App({ 
+  Component, 
+  pageProps: {session, ...pageProps}, 
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
-export default MyApp
+export default App
---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,93 @@
+import NextAuth, { Account, Session, User } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { JWT } from "next-auth/jwt"
+
+  
+async function refreshAccessToken(token: JWT) {
+    try {
+      const url =
+        "https://oauth2.googleapis.com/token?" +
+        new URLSearchParams({
+          client_id: process.env.GOOGLE_CLIENT_ID as string,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+          grant_type: "refresh_token" as string,
+          refresh_token: token.refreshToken as string,
+        })
+  
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        method: "POST",
+      })
+  
+      const refreshedTokens = await response.json()
+  
+      if (!response.ok) {
+        throw refreshedTokens
+      }
+  
+      return {
+        ...token,
+        accessToken: refreshedTokens.access_token,
+        accessTokenExpires: Date.now() + refreshedTokens.expires_at * 1000,
+        refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+      }
+    } catch (error) {
+      console.log(error)
+  
+      return {
+        ...token,
+        error: "RefreshAccessTokenError",
+      }
+    }
+  }
+
+export default NextAuth({
+
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                url: "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code" as string,
+                params: {
+                    scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly"
+                }
+            }
+        })
+    ],
+    secret: process.env.NEXT_AUTH_SECRET,
+    jwt: {
+        secret: process.env.NEXT_AUTH_SECRET
+    },
+    callbacks: {
+        async jwt({ token, user, account}): Promise<JWT> {
+            let exp = account?.expires_at ?? 0;
+            if (account && user) {
+            return {  
+              accessToken: account.access_token,
+              accessTokenExpires: Date.now() + exp * 1000,
+              refreshToken: account.refresh_token,
+              user
+            }
+          }
+    
+          // Return previous token if the access token has not expired yet
+          if (account?.expires_at === null || Date.now() < token.accessTokenExpires) {
+            return token
+          }
+    
+          // Access token has expired, try to update it
+          return refreshAccessToken(token)
+        },
+        async session({ session, token }: {session: Session, token: JWT}): Promise<Session> {
+          session.user = token.user
+          session.accessToken = token.accessToken
+          session.error = token.error
+    
+          return session
+        },
+      }
+});
+
